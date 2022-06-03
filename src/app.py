6a875058ffc01f954481a56a9a1b90a18e01541e
y c <40169115+extreme4all@users.noreply.github.com>
2022-06-03 20:36:46 +0200

    added scraper

--- src/app.py
+++ src/app.py
@@ -1,7 +1,15 @@
+import logging
+
+import aiohttp
 from fastapi import status
 from fastapi.exceptions import HTTPException
 
+from src.cogs.scraper import Scraper
 from src.config import app
+
+logger = logging.getLogger(__name__)
+
+scraper = Scraper(proxy="")
 
 
 @app.get("/")
@@ -32,7 +40,11 @@ async def get_contributions(version, contributor):
 
 @app.get("/{version}/site/prediction/{player_name}")
 async def get_prediction(player_name, version=None, token=None):
-    # TODO: scrape hiscores
+    # scrape hiscores
+    player = {"id": 1, "name": player_name}
+    async with aiohttp.ClientSession() as session:
+        player_data = await scraper.lookup_hiscores(player=player, session=session)
+    logger.debug(player_data)
     # TODO: parse data
     # TODO: post to ML
     # TODO: return prediction
--- src/config.py
+++ src/config.py
@@ -1,3 +1,23 @@
 from fastapi import FastAPI
+import logging
+import sys
 
 app = FastAPI()
+
+
+# setup logging
+file_handler = logging.FileHandler(filename="error.log", mode="a")
+stream_handler = logging.StreamHandler(sys.stdout)
+
+# log formatting
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(funcName)s - %(levelname)s - %(message)s"
+)
+file_handler.setFormatter(formatter)
+stream_handler.setFormatter(formatter)
+
+handlers = [file_handler, stream_handler]
+
+logging.basicConfig(level=logging.DEBUG, handlers=handlers)
+
+logging.getLogger("urllib3").setLevel(logging.INFO)
